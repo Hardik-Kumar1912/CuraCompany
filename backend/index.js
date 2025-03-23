6f1a7ed3a0a3424a322e14798a04f0a7c28d8bfd
Hardik-Kumar1912 <hardikk1401@gmail.com>
2025-03-23 12:45:28 +0530

    Merging frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import express from "express"; 
 import dotenv from "dotenv";
+import path from "path";
 import cookieParser from "cookie-parser";
 import connectDB from "./db/connectDB.js";
 import authRoutes from "./routes/auth.routes.js";
 import testRoutes from "./routes/test.routes.js";
 
 dotenv.config();
+
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
@@ -16,6 +19,12 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tests", testRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend" , "dist", "index.html"));
+});
 
 connectDB().then(() => {
     app.listen(PORT, () => {
